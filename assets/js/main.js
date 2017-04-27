e7885c3773ec0ac1abd63d2c710bbd87d5eccd2f
Anayn Pavez <nanyn.pavez@gmail.com>
2017-04-27 10:48:37 -0300

    segunda version: donde se pueden ver los titles en una lista ordenada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-alert("hola mundo");
+
+function Identificador() {
+	this.userId = userId;
+	this.id = id;
+	this.title = title;
+	this.completed = completed;
+}
+
+function botonFuncional() {
+	var boton = document.getElementsById('boton');
+}
